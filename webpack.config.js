87b42b22dc0e4fa42971101895ffc6b81bd21699
brmedeiros <br.medeiros@gmail.com>
2017-12-02 15:44:03 -0300

    add bootstrap setup to webpack.config, app.js and index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: './src/js/app.js',
@@ -16,7 +18,24 @@ module.exports = {
 		options: {
 		    presets: ['env', 'react']
 		}
+	    },
+
+	    {
+	    	test: /\.css$/,
+	    	use: ExtractTextPlugin.extract({
+	    	    fallback: "style-loader",
+	    	    use: "css-loader"
+	    	})
 	    }
 	]
-    }
+    },
+    plugins: [
+	new ExtractTextPlugin("main.css"),
+	new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery",
+	    Popper: ['popper.js', 'default']
+	})
+    ]
 };
