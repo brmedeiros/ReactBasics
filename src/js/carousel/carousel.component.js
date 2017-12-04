cf28edcfc8239cc4d41e443abedcd1dc3d066bf5
brmedeiros <br.medeiros@gmail.com>
2017-12-03 21:03:06 -0300

    add captions to the carousel component

--- src/js/carousel/carousel.component.js
+++ src/js/carousel/carousel.component.js
@@ -3,25 +3,35 @@ import React from 'react';
 export default class Carousel extends React.Component {
     render() {
 	return(
-	    <div id='carouselExampleControls' className='carousel slide' data-ride='carousel'>
+	    <div id='carouselControls' className='carousel slide container mx-auto' data-ride='carousel' data-interval='5000'>
 	      <div className='carousel-inner'>
 		<div className='carousel-item active'>
 		  <img className='d-block w-100' src='dist/assets/carousel/gruyere.jpg' alt='First slide'/>
+		  <div className="carousel-caption">
+		    <h4>First Thumbnail label</h4>
+		    <p>This is the caption</p>
+		  </div>
 		</div>
 		<div className='carousel-item'>
 		  <img className='d-block w-100' src='dist/assets/carousel/chasm.jpg' alt='Second slide'/>
+		  <div className="carousel-caption">
+		    <h4>Second Thumbnail label</h4>
+		    <p>This is the caption</p>
+		  </div>
 		</div>
 		<div className='carousel-item'>
 		  <img className='d-block w-100' src='dist/assets/carousel/seaside.jpg' alt='Third slide'/>
+		  <div className="carousel-caption">
+		    <h4>Third Thumbnail label</h4>
+		    <p>This is the caption</p>
+		  </div>
 		</div>
 	      </div>
-	      <a className='carousel-control-prev' href='#carouselExampleControls' role='button' data-slide='prev'>
-		<span className='carousel-control-prev-icon' aria-hidden='true'></span>
-		<span className='sr-only'>Previous</span>
+	      <a className='carousel-control-prev' href='#carouselControls' role='button' data-slide='prev'>
+		<span className='carousel-control-prev-icon'></span>
 	      </a>
-	      <a className='carousel-control-next' href='#carouselExampleControls' role='button' data-slide='next'>
-		<span className='carousel-control-next-icon' aria-hidden='true'></span>
-		<span className='sr-only'>Next</span>
+	      <a className='carousel-control-next' href='#carouselControls' role='button' data-slide='next'>
+		<span className='carousel-control-next-icon'></span>
 	      </a>
 	    </div>
 	);
